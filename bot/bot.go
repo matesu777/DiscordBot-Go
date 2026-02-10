6089bf049b9a7baaf8c12be723666f63255ae326
Maateuss77 <programadormateusa@gmail.com>
2026-02-09 21:06:18 -0300

    Organize project

---
+++ bot/bot.go
@@ -0,0 +1,36 @@
+package bot
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func Start(){
+	godotenv.Load()
+	TOKEN := os.Getenv("TOKEN")
+	dg, err := discordgo.New("Bot " + TOKEN)
+	if err != nil{
+		fmt.Println("Erro ao criar session com discord: ", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	fmt.Println("Bot is now running")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	dg.Close()
+}
